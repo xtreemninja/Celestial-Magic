3f350078f008ba93a4f2ca45ffcb09d86cbf1fa4
Xtreemninja <konfoninja5@gmail.com>
2015-05-14 15:38:01 -0600

    hi

---
+++ src/main/java/com/Xtreemninja/CelestialMagic/CelestialMagic.java
@@ -0,0 +1,30 @@
+package com.Xtreemninja.CelestialMagic;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="CelestialMagic" , name="CelestialMagic" , version ="1.7.10-1.0")
+public class CelestialMagic
+{
+    @Mod.Instance("CelestialMagic")
+    public static CelestialMagic instance;
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
